a5a4f591812729cadbc10eb50572770e841438de
Manny Isles <mannysoft@gmail.com>
2016-12-21 11:13:21 +0800

    updates

---
+++ tests/angular/app/components/create_post_form1.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test create-post-form1 component',
+    modules: 'app',
+    element: '<create-post-form1></create-post-form1>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
