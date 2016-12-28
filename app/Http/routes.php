e971c8f2727b5369793759583fa6c4dc84d036ed
Manny Isles <mannysoft@gmail.com>
2016-12-28 13:21:55 +0800

    Evaluation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,44 @@
 |
 */
 
+Route::get('graph', function() {
+    # This script can draw different charts depending on the chartIndex
+    $chartIndex = (int)(request("img"));
+
+    # The data for the pie chart
+    $data = array(25, 18, 15, 12, 8, 30, 35);
+
+    # The labels for the pie chart
+    $labels = array("Labor", "Licenses", "Taxes", "Legal", "Insurance", "Facilities", "Production");
+
+    # Create a PieChart object of size 280 x 240 pixels
+    $c = new PieChart(280, 240);
+
+    # Set the center of the pie at (140, 130) and the radius to 80 pixels
+    $c->setPieSize(140, 130, 80);
+
+    # Add a title to the pie to show the start angle and direction
+    if ($chartIndex == 0) {
+        $c->addTitle("Start Angle = 0 degrees\nDirection = Clockwise");
+    } else {
+        $c->addTitle("Start Angle = 90 degrees\nDirection = AntiClockwise");
+        $c->setStartAngle(90, false);
+    }
+
+    # Draw the pie in 3D
+    $c->set3D();
+
+    # Set the pie data and the pie labels
+    $c->setData($data, $labels);
+
+    # Explode the 1st sector (index = 0)
+    $c->setExplode(0);
+
+    # Output the chart
+    header("Content-type: image/png");
+    print($c->makeChart2(PNG));
+});
+
 Route::get('results/{email}', function($email){
     $user = \App\Modal\User::where('email', $email)->first();
     $evaluates = \App\Modal\Evaluate::where('take_user_id', $user->id)->orderBy('created_at', 'desc')->get();
@@ -103,6 +141,9 @@ $api->group(['middleware' => ['api', 'api.auth']], function ($api) {
 
     // Users
     $api->get('users', 'MIUserController@index');
+
+    // Elements
+    $api->get('elements', 'MIElementController@index');
     
 
 });
